23fcca3f7b9f54d357a0d37bc8bb003829ecf4c3
kompiuteris <kristijonas9871@gmail.com>
2022-12-27 16:46:38 +0200

    Added [post table

--- Assets/TableToSheet.cs
+++ Assets/TableToSheet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Networking;
 
 public class TableToSheet : MonoBehaviour
 {   
@@ -17,6 +18,7 @@ public class TableToSheet : MonoBehaviour
         cells = table.GetCells();
         filepath = Application.dataPath + "/" + fileName;
         Invoke("WriteCSV",5);
+        Invoke("SaveToGoogleSheets",3);
     }
     public void SaveToCSV()
     {
@@ -43,5 +45,30 @@ public class TableToSheet : MonoBehaviour
         }
         tw.Close();
 
+    }
+    void SaveToGoogleSheets()
+    {
+      //  PUT https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1!A1:D5?valueInputOption=USER_ENTERED
+      //  PUT https://sheets.googleapis.com/v4/spreadsheets/1nvF0ZXtj76Hkky5EIYMTS1kuoO5k32SpvwgAYKPYPXE/values/Sheet1!A1:D5?valueInputOption=USER_ENTERED
+
+      //https://docs.google.com/spreadsheets/d/1nvF0ZXtj76Hkky5EIYMTS1kuoO5k32SpvwgAYKPYPXE/edit#gid=0
+      Upload();
+    }
+    IEnumerator Upload()
+    {
+        byte[] myData = System.Text.Encoding.UTF8.GetBytes("This is some test data");
+        using (UnityWebRequest www = UnityWebRequest.Put("https://sheets.googleapis.com/v4/spreadsheets/1nvF0ZXtj76Hkky5EIYMTS1kuoO5k32SpvwgAYKPYPXE/values/Sheet1!A1:D5?valueInputOption=USER_ENTERED", myData))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Upload complete!");
+            }
+        }
     }
 }
